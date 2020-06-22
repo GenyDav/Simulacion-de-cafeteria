66a72e17a3ac3343f9d920688a161d1d5af877c3
GD <geny.d.mtz@hotmail.com>
2020-06-22 18:39:52 -0500

    Panel para mostrar datos añadido

--- Escena.java
+++ Escena.java
@@ -5,11 +5,12 @@
  */
 package cafeteria;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.border.Border;
 
 /**
  *
@@ -18,15 +19,22 @@ import javax.swing.border.EmptyBorder;
 public class Escena {
     JFrame marco;
     JPanel lienzo;
+    JPanel contador;
     Escena(){
         marco = new JFrame("Café");
         marco.setDefaultCloseOperation(EXIT_ON_CLOSE);
         lienzo = new Fondo("cafeteria/sprites/fondo.png");
+        contador = new JPanel();
+        contador.setBackground(Color.DARK_GRAY);
+        contador.setSize(new Dimension(566,20));
+        marco.add(contador);
         marco.setSize(new Dimension(566,457));
-        marco.setContentPane(lienzo);
+        //marco.setContentPane(lienzo);
+        marco.add(lienzo);
         marco.setLocationRelativeTo(null);
         marco.setVisible(true);
     }
+    
     public static void main(String []args){
         Escena e = new Escena();
         
