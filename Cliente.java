ec650709510bef2c473f9ff6258b040946d94690
GD <geny.d.mtz@hotmail.com>
2020-07-11 21:47:04 -0500

    Conteo de tiempo para el pedido del cliente

--- Cliente.java
+++ Cliente.java
@@ -13,6 +13,8 @@ public class Cliente {
     private int estado;
     private float velocidad;
     private BufferedImage sprt1,sprt2,sprt3,sprtActual; //sprites del personaje
+    private float tPedido; // tiempo que tardará el pedido del cliente (segundos)
+    private float transcurrido;
     
     public Cliente(){
         sprt1 = Imagen.cargaImagen("cafeteria/sprites/lady5.png");
@@ -24,6 +26,8 @@ public class Cliente {
         y = 254;
         velocidad = 80;
         estado = 1;
+        tPedido = 5;
+        transcurrido = 0;
     }
     
     public void pintarCliente(Graphics g){
@@ -36,25 +40,30 @@ public class Cliente {
             case 1:
                 if(x>168){
                     x -= velocidad*dt;
-                    System.out.println(dt);
+                    //System.out.println(dt);
                 }else{
                     sprtActual = sprt2;
                     estado = 2;
                 }
                 break;
             case 2:
-                try{                
+                /*try{                
                     Thread.sleep(1000);
-                }catch(InterruptedException e){}
-                System.out.println("Despertando");
-                System.out.println("Cambiando a 3");
-                sprtActual = sprt3;
-                estado = 3;
+                }catch(InterruptedException e){}*/
+                
+                //System.out.println("Despertando");
+                //System.out.println("Cambiando a 3");
+                transcurrido += dt;
+                System.out.println(transcurrido);
+                if(transcurrido>tPedido){
+                    sprtActual = sprt3;
+                    estado = 3;
+                }
                 break;
             case 3:
-                System.out.println(y);
+                //System.out.println(y);
                 if(y<418){
-                    System.out.println("sleep: "+dt);
+                    //System.out.println("sleep: "+dt);
                     y += velocidad*dt;
                 }
                 break;
