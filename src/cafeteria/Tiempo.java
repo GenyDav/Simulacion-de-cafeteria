2110dac40365afe54f68ddd2b9bcf74a2b9a0736
GD <geny.d.mtz@hotmail.com>
2021-12-19 14:57:38 -0600

    Documentación de la clase Tiempo

--- src/cafeteria/Tiempo.java
+++ src/cafeteria/Tiempo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cafeteria;
 
 import java.awt.event.ActionEvent;
@@ -11,17 +6,21 @@ import javax.swing.JLabel;
 import javax.swing.Timer;
 
 /**
- *
- * @author Geny
+ * Clase para llevar el tiempo transcurrido durante la ejecución de la simulación
+ * @author Geny D.
+ * @version 1.0
  */
 public class Tiempo {
-    private int min;
-    private int seg;
-    private Timer timer;
-    private JLabel etiqueta;
-    private String txtMin, txtSeg;
+    private int min;                // minutos transcurridos 
+    private int seg;                // segundos transcurridos
+    private Timer timer;            // objeto que se ejecuta cada 1000ms y modifica los demas atributos
+    private JLabel etiqueta;        // referencia que apunta a la equiteta de la clase Main
+    private String txtMin, txtSeg;  // valores mostrados en la etiqueta
     Reloj r;
     
+    /**
+     * Constructor
+     */
     public Tiempo(){
         r = new Reloj();
         min = 0;
@@ -36,29 +35,46 @@ public class Tiempo {
                 }
                 txtMin = min<10?"0"+Integer.toString(min):Integer.toString(min);
                 txtSeg = seg<10?"0"+Integer.toString(seg):Integer.toString(seg);            
-                //System.out.println(min+":"+seg);
+                
                 etiqueta.setText("Tiempo: "+txtMin+":" + txtSeg);
-                r.moverManecillas(min, seg);
+                r.moverManecillas(min, seg);    // pintar las manecillas del reloj según el tiempo
             }
         });  
     } 
     
+    /**
+     * Método de devuelve el número de minutos transcurridos desde el inicio de la simulación
+     * @return minutos transcurridos
+     */
     public int getMin(){
         return min;
     }
     
+    /**
+     * Método que devuelve el número de segundos transcurridos asociados con un minuto
+     * @return segundos transcurridos
+     */
     public int getSeg(){
         return seg;
     }
     
+    /**
+     * Inicia el conteo del tiempo
+     */
     public void iniciarTiempo(){
         timer.start();
     }
     
+    /**
+     * Detiene el conteo del tiempo
+     */
     public void detenerTiempo(){
         timer.stop();
     }
     
+    /**
+     * Reinicia todos los valores de tiempo
+     */
     public void reiniciarContador(){
         min = 0;
         seg = 0;
@@ -66,10 +82,18 @@ public class Tiempo {
         r.obtenerHoraSistema();
     }
     
+    /**
+     * Método que establece la referencia a el objeto JLabel del parámetro
+     * @param label etiqueta en donde se muestra el tiempo 
+     */
     public void setEtiqueta(JLabel label){
         etiqueta = label;
     }
     
+    /**
+     * Devuelve el objeto para representar el gráfico de un reloj
+     * @return el objeto para el gráfico de tipo Reloj
+     */
     public Reloj getReloj(){
         return r;
     }
