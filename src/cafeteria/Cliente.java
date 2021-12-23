49379cc64bb42ff2e279a6294c1a5066fff06f22
GD <geny.d.mtz@hotmail.com>
2021-12-23 16:58:17 -0600

    Documentación de la clase Cliente.java

--- src/cafeteria/Cliente.java
+++ src/cafeteria/Cliente.java
@@ -5,195 +5,217 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 /**
- *
- * @author Geny
+ * Clase que crea un objeto que representa al cliente. El cual, a partir de que
+ * es creado, se forma en la fila esperando su turno para ser atendido, mostrando
+ * una barra con el tiempo de espera restante. Si el tiempo que puede esperar se
+ * termina, el cliente abandona la fila y sale del lugar. Si el cliente es el 
+ * primero de la fila y el ojeto de la clase Consumidor está desocupado, el 
+ * cliente avanza hasta llegar a la posición frente al Consumidor, le entrega su
+ * pedido y espera a que el Consumidor le devuelva el plato preparado, cuando lo
+ * recibe sale del lugar.
+ * @author Geny D.
+ * @version 2.1
  */
-public class Cliente {
-    private float x,y;      // coordenadas del sprite
-    private int cliente;    // tipo de cliente
-    public int estado;
-    public static final int CTE_ESPERANDO_FILA = 1;
-    public static final int CTE_ESPERANDO_CHEF = 2;
-    public static final int CTE_SALIENDO_LUGAR = 3;
+public final class Cliente {
+    private float x,y;                                  // coordenadas del sprite del cliente
+    private int cliente;                                // número de identificación del cliente
+    public int estado;                                  
+    public static final int CTE_ESPERANDO_FILA = 1; // estado del cliente cuando está formado en la fila
+    public static final int CTE_ESPERANDO_CHEF = 2; // estado del cliente cuando está frente al cocinero(chef) esperando su pedido
+    public static final int CTE_IMPACIENTE = 3;     // estado del cliente cuando se acaba su tiempo de espera
+    public static final int CTE_ATENDIDO = 4;       // estado del cliente cuando se le entrega su pedido listo
     private float velocidad;
-    private BufferedImage sprt1,sprt2,sprt3,sprtActual;  //sprites del personaje
-    //private BufferedImage pedido;   // sprite del pedido del cliente
-    Pedido platoListo;
-    private double spSalida;
-    //double tPedido;         // tmpPedido que tardará el pedido del cliente (segundos)
-    //private double transcurrido;
-    public double tmpEspera;       // tmpPedido que el cliente puede esperar en la fila
-    private double tmpAux;          // variables usada para la barra de progreso de tmpPedido en la fila
-    public double porcentaje, porcentajePedido;
-    private boolean saliendo;
-    private boolean atendido;
-    private boolean terminado;
-    private int plato;
+    private BufferedImage sprt2,sprtActual; //sprites del personaje
+    private int plato;                      // núero de identificación del plato que va a ordenar el cliente
+    Pedido platoListo;                      // plato que el cocinero le entrega al cliente cuando está listo
+    private double spSalida;                // número del sprite que se va a mostrar en pantalla
+    public double tmpRestante;              // tiempo que le queda al cliente antes de salir de la fila
+    private double tmpEspera;               // tiempo que el cliente puede esperar en la fila
+    public double porcentaje;               // porcentaje del tiempo de espera del cliente
+    private boolean cteFuera;               // Indica si el cliente está dentro o fuera de la cafetería
     
-    /*
-    public Cliente(){
-        sprt1 = Imagen.cargaImagen("cafeteria/sprites/lady_1.png");
-        sprt2 = Imagen.cargaImagen("cafeteria/sprites/lady9.png");
-        spSalida = 1;
-        sprtActual = sprt1;
-        x = 550;
-        y = 254;
-        velocidad = 80;
-        estado = 1;
-        this.tmpEspera = 0;
-        //transcurrido = 0;
-        saliendo = false;
-        porcentaje = 1.0;
-        porcentajePedido = 0;
-        //pedido = Imagen.cargaImagen("cafeteria/sprites/plato"+plato+".png");
-        atendido = false;
-    }*/
-    
+    /**
+     * Constructor
+     * @param tmpEspera tiempo que el cliente puede esperar en la fila
+     * @param plato número de identificación del plato a pedir
+     * @param cliente número de identificación para seleccionar el sprite del cliente
+     */
     public Cliente(double tmpEspera,int plato,int cliente){
-        sprt1 = Imagen.cargaImagen("sprites/"+cliente+"_1.png");
-        sprt2 = Imagen.cargaImagen("sprites/"+cliente+"9.png");
-        spSalida = 1;
-        sprtActual = sprt1;
-        x = 550;
-        y = 255;
-        velocidad = 80;
-        estado = 1;
-        //this.tPedido = tPedido;
         this.tmpEspera = tmpEspera;
-        tmpAux = tmpEspera;
-        //transcurrido = 0;
-        saliendo = false;
-        porcentaje = 1.0;
-        porcentajePedido = 0;
-        //pedido = Imagen.cargaImagen("cafeteria/sprites/plato"+plato+".png");
-        this.cliente = cliente; 
-        atendido = false;
-        terminado = false;
+        tmpRestante = tmpEspera;
         this.plato = plato;
+        this.cliente = cliente; 
+        sprt2 = Imagen.cargaImagen("sprites/"+cliente+"9.png");
+        spSalida = 1;                   // sprite con el personaje de frente
+        sprtActual = null;
+        x = 550;                        // coordenada x inicial
+        y = 255;                        // coordenada y inicial
+        velocidad = 65;                 // distancia que avanza el sprite del cliente cuando se dibuja en pantalla
+        estado = CTE_ESPERANDO_FILA;       
+        porcentaje = 1.0;               // porcentaje del tiempo restante (100%)
+        cteFuera = false;
     }
     
-    public void pintarCliente(Graphics g){
-        g.drawImage(sprtActual,Math.round(x),Math.round(y),null);
-        if(estado<2){ // mostrar barra de tmpPedido restante de espera
-            g.setColor(Color.WHITE);
-            g.fillRect(Math.round(x),Math.round(y-10),40,5);
-            g.setColor(Color.BLUE);
-            g.fillRect(Math.round(x),Math.round(y-10), (int)Math.round(porcentaje*40),5);
-        }/*else if(estado==2){ // mostrar una barra de progreso en el pedido
-            /*g.setColor(Color.WHITE);
-            g.fillRect(Math.round(x),Math.round(y-80),40,5);
-            g.setColor(Color.RED);
-            g.fillRect(Math.round(x),Math.round(y-80),(int)Math.round(porcentajePedido*40),5);
-            //g.drawImage(pedido,Math.round(x+3),Math.round(y-105),null);
-        }*/
-        else if(estado==CTE_SALIENDO_LUGAR){
-            if(atendido){
-                g.setColor(Color.WHITE);
-                g.drawString("¡Gracias!", Math.round(x-3), Math.round(y-32));
-                g.drawImage(platoListo.getSprite(),Math.round(x+5),Math.round(y-32),null);
-            }else if(saliendo){
-                g.setColor(Color.WHITE);
-                g.drawString("¡Me voy!", Math.round(x), Math.round(y-10));
-            }
-        }
-    }
-    
-    public void restarTiempoEspera(float dt){
-        tmpEspera -= dt;
-        porcentaje = tmpEspera/tmpAux;
-        //System.out.println("tmpPedido: "+tmpEspera);
-        if(tmpEspera<=0){
-            //sprtActual = sprt3;
-            estado = CTE_SALIENDO_LUGAR;
-            saliendo = true;
-        }
-    }
-    
-    public void serAtendido(float dt){
-        //System.out.println("Siendo atendido");
-        switch(estado){
-            case CTE_ESPERANDO_FILA:
-                if(x>168){
-                    x -= velocidad*dt;
-                    sprtActual = Imagen.cargaImagen("sprites/"+cliente+"_"+(int)spSalida+".png");
-                    //System.out.println((int)spSalida);
-                    spSalida += 0.1;
-                    if(spSalida>5){
-                        spSalida = 1;
-                    }
-                }else{
-                    sprtActual = sprt2;
-                    estado = CTE_ESPERANDO_CHEF;
-                    Consumidor.cocinando = true;
-                }
-                break;
-            case CTE_ESPERANDO_CHEF:
-                //Consumidor.cocinando = true;
-                /*transcurrido += dt;
-                porcentajePedido = transcurrido/tPedido;
-                if(transcurrido>tPedido){
-                    estado = 3;
-                    Consumidor.cocinando = false;
-                }*/
-                atendido = true;
-                break;
-            case CTE_SALIENDO_LUGAR: // saliendo del lugar
-                //System.out.println(y);
-                salir(dt);
-                break;
-        }
-    }
-
-    public boolean getTerminado() {
-        return terminado;
-    }
-    
-    public int getEstado(){
-        return estado;
-    }
-    
+    /**
+     * Modifica el estado del cliente según el valor entero pasado como parámetro:
+     * @param estado estado del cliente
+     */
     public void setEstado(int estado){
         this.estado = estado;
     }
     
-    public boolean getSalida(){
-        return saliendo;
+    /**
+     * Método utilizado para obtener el estado del cliente
+     * @return estado del cliente
+     */
+    public int getEstado(){
+        return estado;
     }
     
+    /**
+     * Método que devuelve el número de identificación del pedido(plato) que
+     * va a ordenar el cliente
+     * @return número de identificación del pedido
+     */
     public int hacerPedido(){
         return plato;
     }
     
+    /**
+     * Método que 'devuelve' al cliente el objeto de tipo Pedido preparado
+     * por el consumidor
+     * @param p plato preparado para el cliente 
+     */
     public void recibirPlato(Pedido p){
         platoListo = p;
     }
     
-    public void salir(float dt){
-        if(y<458){
-            y += velocidad*dt;
-            sprtActual = Imagen.cargaImagen("sprites/"+cliente+(int)spSalida+".png");
-            //System.out.println("conteo:"+spSalida);
-            spSalida += 0.1;
-            if(spSalida>5){
-                spSalida = 1;
-            }
-        }    
-        else{
-            terminado = true;
+    /**
+     * Regresa un valor booleano que indica si el cliente aún está dentro de
+     * la cafetería(false) o ya salió de ella(true).
+     * @return
+     */
+    public boolean getClienteFuera() {
+        return cteFuera;
+    }
+    
+    /**
+     * Dibuja el sprite del cliente en pantalla según su estado. Tambien 
+     * dibuja el sprite del pedido cuando está listo y muestra los mensajes
+     * sobre el cliente cuando es necesario.
+     * @param g Contexto gráfico 
+     */
+    public void pintarCliente(Graphics g){
+        g.drawImage(sprtActual,Math.round(x),Math.round(y),null);   // dibujar al cliente
+        switch(estado){
+            case CTE_ESPERANDO_FILA:
+                // Mostrar barra con el tiempo de espera restante
+                g.setColor(Color.WHITE);
+                g.fillRect(Math.round(x),Math.round(y-10),40,5);
+                g.setColor(Color.BLUE);
+                g.fillRect(Math.round(x),Math.round(y-10), (int)Math.round(porcentaje*40),5);
+                g.drawString(String.valueOf(Math.round(tmpRestante)), Math.round(x), Math.round(y-15));
+                break;
+            case CTE_ATENDIDO:
+                // Mostrar el sprite del pedido sobre el cliente
+                g.setColor(Color.WHITE);
+                g.drawString("¡Gracias!", Math.round(x-3), Math.round(y-32));
+                g.drawImage(platoListo.getSprite(),Math.round(x+5),Math.round(y-32),null);
+                break;
+            case CTE_IMPACIENTE:
+                // Mostrar un mensaje sobre el cliente cuando sale de la fila
+                g.setColor(Color.WHITE);
+                g.drawString("¡Me voy!", Math.round(x), Math.round(y-10));
+                break;
         }
     }
     
+    /**
+     * Método que hace que el cliente se forme en la fila tomando su lugar
+     * correspondiente según su posición al entrar a la simulación y cuando
+     * otro cliente sale de la fila.
+     * cuando entra a la simulación.   
+     * @param dt Diferencia de tiempo recibida desde la clase que lleva el
+     * control de la simulación
+     * @param lugar Posición del cliente en la fila
+     */
     public void avanzarFila(float dt,int lugar){
         if(x>(300+lugar*60)){
             x -= velocidad*dt;
             sprtActual = Imagen.cargaImagen("sprites/"+cliente+"_"+(int)spSalida+".png");
             //System.out.println("conteo:"+spSalida);
-            spSalida += 0.1;
+            spSalida += dt*5;   // hacer que el sprite cambie 5 veces por segundo
             if(spSalida>5){
                 spSalida = 1;
             }
         }else{
             sprtActual = Imagen.cargaImagen("sprites/"+cliente+"_3.png");
+        }
+    }
+    
+    /**
+     * Método que va restando el tiempo que el cliente puede permanecer en la
+     * fila esperando a ser atendido. Cuando el tiempo de espera del cliente
+     * llega a 0, el cliente se considera como perdido.
+     * @param dt cantidad de tiempo en milisegundos pasada como parámetro desde
+     * el ciclo que lleva el control de la simulación
+     */
+    public void restarTiempoEspera(float dt){
+        tmpRestante -= dt;
+        porcentaje = tmpRestante/tmpEspera;
+        if(tmpRestante<=0){
+            estado = CTE_IMPACIENTE;
+        }
+    }
+    
+    /**
+     * Método que define los sprites del cliente que van a ser mostrados cuando
+     * éste deja la fila para hacer el pedido al objeto Consumidor, cuando está
+     * esperando a que el pedido esté listo, y cuando el cliente recibe el 
+     * pedido y sale del lugar.
+     * @param dt Diferencia de tiempo recibida desde la clase que lleva el
+     * control de la simulación
+     */
+    public void serAtendido(float dt){
+        switch(estado){
+            case CTE_ESPERANDO_FILA:
+                // Avanzar hasta donde está el cocinero(Consumidor)
+                if(x>150){
+                    x -= velocidad*dt;
+                    sprtActual = Imagen.cargaImagen("sprites/"+cliente+"_"+(int)spSalida+".png");
+                    //System.out.println((int)spSalida);
+                    spSalida += dt*5;
+                    if(spSalida>5){
+                        spSalida = 1;
+                    }
+                }else{  // Cuando el cliente está frente al cocinero              
+                    sprtActual = sprt2;
+                    estado = CTE_ESPERANDO_CHEF;
+                    Consumidor.cocinando = true;
+                }
+                break;
+            case CTE_ATENDIDO:
+                salir(dt);
+                break;
+        }
+    }        
+    
+    /**
+     * Método que muestra la animación del cliente saliendo de la cafetería
+     * @param dt Diferencia de tiempo recibida desde la clase que lleva el
+     * control de la simulación.
+     */
+    public void salir(float dt){
+        if(y<458){
+            y += velocidad*dt;  // calcular la nueva posición del cliente
+            sprtActual = Imagen.cargaImagen("sprites/"+cliente+(int)spSalida+".png");
+            //System.out.println("conteo: "+spSalida);
+            spSalida += dt*5;   // hacer que el sprite cambie 5 veces por segundo
+            if(spSalida>5){
+                spSalida = 1;
+            }
+        }else{
+            cteFuera = true;
         }
     }
 }
